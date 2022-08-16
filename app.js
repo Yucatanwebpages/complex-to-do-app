5e76a1ab98227fb1193161aa5bff4e4ebd57e092
Piotr <websites@hello-24.com>
2022-08-15 22:19:17 -0500

    Update files

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 'use strict';
 
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
 const express = require('express');
 const app = express();
+
+let sessionOption = session({
+  secret: 'JavaScript is so cool',
+  store: MongoStore.create({
+    client: require('./db'),
+  }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 1000 * 60 * 60 * 24, httpOnly: true },
+});
+
+app.use(sessionOption);
+app.use(flash());
+
 const router = require('./router.js');
 
 app.use(express.urlencoded({ extended: false }));
--- controllers/userControler.js
+++ controllers/userControler.js
@@ -7,25 +7,58 @@ exports.login = function (req, res) {
   user
     .login()
     .then(function (result) {
-      res.send(result);
+      req.session.user = { avatar: user.avatar, username: user.data.username };
+      req.session.save(function () {
+        res.redirect('/');
+      });
     })
     .catch(function (e) {
-      res.send('e');
+      req.flash('errors', e);
+      req.session.save(function () {
+        res.redirect('/');
+      });
     });
 };
 
-exports.logout = function () {};
+exports.logout = function (req, res) {
+  req.session.destroy(function () {
+    res.redirect('/');
+  });
+};
 
 exports.register = function (req, res) {
   let user = new User(req.body);
-  user.register();
-  if (user.errors.length) {
-    res.send(user.errors);
-  } else {
-    res.send('Congrats, there is no errors.');
-  }
+  user
+    .register()
+    .then(() => {
+      req.session.user = {
+        username: user.data.username,
+        avatar: user.avatar,
+      };
+      req.session.save(function () {
+        res.redirect('/');
+      });
+    })
+    .catch((regErrors) => {
+      regErrors.forEach(function (error) {
+        req.flash('regErrors', error);
+      });
+      req.session.save(function () {
+        res.redirect('/');
+      });
+    });
 };
 
 exports.home = function (req, res) {
-  res.render('home-guest');
+  if (req.session.user) {
+    res.render('home-dashboard', {
+      username: req.session.user.username,
+      avatar: req.session.user.avatar,
+    });
+  } else {
+    res.render('home-guest', {
+      errors: req.flash('errors'),
+      regErrors: req.flash('regErrors'),
+    });
+  }
 };
